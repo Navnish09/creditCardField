70f12fed9e4e69a49f43d46a7a510e6471959997
Navnish SIngh <navnish@gmail.com>
2021-07-02 16:00:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import Card from './component/Card/Card';
+import List from './component/List/List';
+import ListItem from './component/ListItem/ListItem';
+import  "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {   
+    const [listItems, setListItems] = useState([]);
+
+        
+    const setItems = useCallback((value)=> {
+        setListItems(p => [...p, value]);
+    })
+
+    useEffect(() => {
+        console.log(listItems);
+    })
+
+    return (
+        <> 
+            <div className="container">
+                <div className="main">
+                    <Card setList={setItems} />
+                    <div>
+
+                    <List>
+                        {
+                            listItems.map((item, index) => (
+                                <ListItem key={index}>
+                                    {item}
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
+
 
 export default App;
